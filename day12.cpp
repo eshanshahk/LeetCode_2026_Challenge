12a8246805e81c07adcc9bfd2c3d836c73c50109
eshanshahk <eshanshahk123@gmail.com>
2026-01-25 15:24:52 +0530

    day 12 done

---
+++ day12.cpp
@@ -0,0 +1,17 @@
+//pattern type 4
+#include<iostream>
+using namespace std;
+int main(){
+    int n,num=1;
+    cin>>n;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j <= i; j++)
+        {
+            cout<<num<<"\t";
+        }
+        num++;
+        cout<<"\n";
+    }
+    return 0;
+}
