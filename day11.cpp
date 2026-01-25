a3e0410116eaa4a452fdb68217235698ba480aff
eshanshahk <eshanshahk123@gmail.com>
2026-01-25 15:21:30 +0530

    day11 done

---
+++ day11.cpp
@@ -0,0 +1,16 @@
+//pattern type 3
+#include<iostream>
+using namespace std;
+int main(){
+    int n,num=1;
+    cin>>n;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j <= i; j++)
+        {
+            cout<<"*";
+        }
+        cout<<"\n";
+    }
+    return 0;
+}
